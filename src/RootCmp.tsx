1370a7761e8236f653b71a0f3f001dd2ebe64d5f
OriHaguel <orihaguel@gmail.com>
2024-11-30 17:57:32 +0200

    reduce delay

--- src/RootCmp.tsx
+++ src/RootCmp.tsx
@@ -12,31 +12,26 @@ import { useEffect } from 'react'
 
 export function RootCmp() {
     const location = useLocation();
-    const { data: user, isLoading } = userService.useAuthUser();
+    const { data: user } = userService.useAuthUser();
     const showNavbar = location.pathname !== '/' && location.pathname !== '/auth';
 
     useEffect(() => {
         if (user) {
-
             userService.saveLoggedinUser(user.user)
         }
     }, [user]);
 
-    // Show appropriate component while loading based on location
-    if (isLoading) {
-        if (location.pathname === '/auth') {
-            return <AuthPage />;
-        }
-        if (location.pathname === '/') {
-            return <Home />;
-        }
-        return null
-    }
-
-    // Handle auth-required routes
+    // Handle auth-required routes immediately
     if ((location.pathname === '/' || location.pathname === '/auth') && user) {
         return <Navigate to="/dashboard" replace />;
+    }
 
+    // Public routes should render immediately
+    if (location.pathname === '/auth') {
+        return <AuthPage />;
+    }
+    if (location.pathname === '/') {
+        return <Home />;
     }
 
     return (
